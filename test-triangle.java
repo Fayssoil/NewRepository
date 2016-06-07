d3909455878a420176a19a1a3f016507a756df9b
Fayssoil <fayys@hotmail.fr>
2016-06-07 16:19:16 +0200

    initialisation projet

---
+++ test-triangle.java
@@ -0,0 +1,40 @@
+public class test{
+    
+    
+    public static  boolean estQuelconque(int a, int b, int c){
+        boolean resultat=false;
+        resultat = !estIsocele(a,b,c);
+        return resultat;
+    }
+    public static  boolean estEquilateral(int a, int b, int c){
+        boolean resultat=false;
+        resultat= (a==b)&&(b==c);
+        return resultat;
+    }
+    public static  boolean estIsocele(int a, int b, int c){
+        boolean resultat=false;
+        resultat=(a==b)||(a==c)||(b==c);
+        
+        return resultat;
+    }
+    
+    public static void main(String args[]){
+        System.out.println(" le triangle 2 2 3 est isocele ? "+
+                           estIsocele(2,2,3));
+        System.out.println(" le triangle 1 2 3 est isocele ? "+
+                           estIsocele(1,2,3));
+        System.out.println(" le triangle 2 2 3 est equilateral ? "+
+                           estEquilateral(2,2,3));
+        System.out.println(" le triangle 2 2 2 est equilateral ? "+
+                           estEquilateral(2,2,2));
+        System.out.println(" le triangle 2 2 3 est Quelconque? "+
+                           estQuelconque(2,2,3));
+        System.out.println(" le triangle 1 2 3 est quelconque ? "+
+                           estQuelconque(1,2,3));
+      
+    }
+    
+    
+    
+    
+}
